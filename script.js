8a49a803b0e122ad702aa4d332ac3650dee35be5
Ashish Adhikari <amazingashish061@gmail.com>
2021-02-19 22:25:20 +0545

    2nd

---
+++ script.js
@@ -0,0 +1,57 @@
+// let imageLoaded = false;
+// 	$("#image-selector").change(function () {
+// 		imageLoaded = false;
+// 		let reader = new FileReader();
+// 		reader.onload = function () {
+// 			let dataURL = reader.result;
+// 			$("#selected-image").attr("src", dataURL);
+// 			$("#prediction-list").empty();
+// 			imageLoaded = true;
+// 		}
+		
+// 		let file = $("#image-selector").prop('files')[0];
+// 		reader.readAsDataURL(file);
+// 	});
+
+// 	let model;
+// 	let modelLoaded = false;
+// 	$( document ).ready(async function () {
+// 		modelLoaded = false;
+// 		$('.progress-bar').show();
+// 		console.log( "Loading model..." );
+// 		model = await tf.loadGraphModel('model/model.json');
+// 		console.log( "Model loaded." );
+// 		$('.progress-bar').hide();
+// 		modelLoaded = true;
+// 	});
+
+// 	$("#predict-button").click(async function () {
+// 		if (!modelLoaded) { alert("The model must be loaded first"); return; }
+// 		if (!imageLoaded) { alert("Please select an image first"); return; }
+		
+// 		let image = $('#selected-image').get(0);
+		
+// 		// Pre-process the image
+// 		console.log( "Loading image..." );
+// 		let tensor = tf.browser.fromPixels(image, 3)
+// 			.resizeNearestNeighbor([224, 224]) // change the image size
+// 			.expandDims()
+// 			.toFloat()
+// 			.reverse(-1); // RGB -> BGR
+// 		let predictions = await model.predict(tensor).data();
+// 		console.log(predictions);
+// 		let top5 = Array.from(predictions)
+// 			.map(function (p, i) { // this is Array.map
+// 				return {
+// 					probability: p,
+// 					className: TARGET_CLASSES[i] // we are selecting the value from the obj
+// 				};
+// 			}).sort(function (a, b) {
+// 				return b.probability - a.probability;
+// 			}).slice(0, 2);
+
+// 		$("#prediction-list").empty();
+// 		top5.forEach(function (p) {
+// 			$("#prediction-list").append(<li>${p.className}: ${p.probability.toFixed(6)}</li>);
+// 			});
+// 	});
