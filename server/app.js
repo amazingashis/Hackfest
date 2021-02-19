909fca6c35a00dc5f116cbad67d3f0d5f8367aed
awanshrestha <awanshrestha1@gmail.com>
2021-02-19 22:46:53 +0545

    adds server

---
+++ server/app.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+
+app.use(express.static('public'))
+
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname + '/index.html'));
+})
+
+let port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`App is listening at port ${port}`)
+})
