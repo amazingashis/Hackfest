9fbfb95d399ad3bdb16c97bb1d34f8c0198394c8
awanshrestha <awanshrestha1@gmail.com>
2021-02-20 14:21:34 +0545

    Adds image adder

--- server/public/script.js
+++ server/public/script.js
@@ -1,8 +1,15 @@
-async function run(){
+let modelLoaded = false;
+$( document ).ready(async function () {
     const MODEL_URL = '/model.json';
-    const model = await tf.loadLayersModel(MODEL_URL);
-    return model
-}
+	modelLoaded = false;
+    $('.progress-bar').show();
+    document.getElementById('model-status').innerHTML = "Loading model...";
+    model = await tf.loadLayersModel(MODEL_URL);
+    $('.progress-bar').hide();
+    document.getElementById('model-status').innerHTML = "Loaded model...";
+	modelLoaded = true;
+});
+
 run().then((model)=>{
     const img = document.getElementById('img');
     console.log(model.summary());
@@ -30,4 +37,50 @@ run().then((model)=>{
         }
     })
 
+});
+
+
+
+// $("#predict-button").click(async function () {
+// 	if (!modelLoaded) { alert("The model must be loaded first"); return; }
+// 	if (!imageLoaded) { alert("Please select an image first"); return; }
+	
+// 	let image = $('#selected-image').get(0);
+	
+// 	// Pre-process the image
+// 	console.log( "Loading image..." );
+// 	let tensor = tf.browser.fromPixels(image, 3)
+// 		.resizeNearestNeighbor([224, 224]) // change the image size
+// 		.expandDims()
+// 		.toFloat()
+// 		.reverse(-1); // RGB -> BGR
+// 	let predictions = await model.predict(tensor).data();
+// 	console.log(predictions);
+// 	let top5 = Array.from(predictions)
+// 		.map(function (p, i) { // this is Array.map
+// 			return {
+// 				probability: p,
+// 				className: TARGET_CLASSES[i] // we are selecting the value from the obj
+// 			};
+// 		}).sort(function (a, b) {
+// 			return b.probability - a.probability;
+// 		}).slice(0, 2);
+
+// 	$("#prediction-list").empty();
+// 	top5.forEach(function (p) {
+// 		$("#prediction-list").append(`<li>${p.className}: ${p.probability.toFixed(6)}</li>`);
+// 		});
+// });
+
+let imageLoaded = false;
+$("#image-selector").change(function () {
+	let reader = new FileReader();
+	reader.onload = function () {
+		let dataURL = reader.result;
+		$("#selected-image").attr("src", dataURL);
+        imageLoaded = true;
+	}
+	
+	let file = $("#image-selector").prop('files')[0];
+	reader.readAsDataURL(file);
 });
