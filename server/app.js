69af075ee0895e1505d8820e3544805c0c4ec6a3
awanshrestha <awanshrestha1@gmail.com>
2021-02-20 18:12:32 +0545

    adds multiple pages

--- server/app.js
+++ server/app.js
@@ -8,6 +8,10 @@ app.get('/', (req,res) => {
     res.sendFile(path.join(__dirname + '/index.html'));
 })
 
+app.get('/predict', (req,res) => {
+    res.sendFile(path.join(__dirname + '/public/predict.html'));
+})
+
 let port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`App is listening at port ${port}`)
